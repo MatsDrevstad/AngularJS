3255276d08b47f619ccd12d969c0ef72b09d3a4e
MatsDrevstad <mats.drevstad@gmail.com>
2016-02-28 12:51:39 +0100

    Städar upp lite

--- AngularJS/scripts/Script.js
+++ AngularJS/scripts/Script.js
@@ -5,12 +5,12 @@ var myModule = angular.module('myModule', [])
         $scope.message = "Call Of Duty";
 
         var employees = [
-            { firstName: 'Mats', lastName: 'Drevstad', age: 44 },
+            { firstName: 'Mats', lastName: 'Drevstad', age: 45 },
             { firstName: 'Nikolai', lastName: 'Davydenko', age: 12 },
             { firstName: 'Nils', lastName: 'Davydenko', age: 7 },
             { firstName: 'Mirza', lastName: 'Selimasic', age: 47 },
             { firstName: 'Nedisa', lastName: 'Selimasic', age: 13 },
-            { firstName: 'Nedis', lastName: 'Selimasic', age: 10 },
+            { firstName: 'Nedis', lastName: 'Selimasic', age: 11 },
         ];
 
         $scope.employees = employees;
