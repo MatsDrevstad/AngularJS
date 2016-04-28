e98bcff91fcebfad953d8a0941f4483ba5e8b0d4
MatsDrevstad <mats.drevstad@gmail.com>
2016-04-28 20:46:16 +0200

    lesson 03

---
+++ AngularJS/scripts/Script03.js
@@ -0,0 +1,20 @@
+/// <reference path="angular.js" />
+
+//03.html
+//Create the module
+var myApp = angular.module("myModule", []);
+
+//Create the Controller with the Module
+myApp.controller("myController", function ($scope) {
+	var employees = [
+		{ firstName: 'Mats', lastName: 'Drevstad', age: 45 },
+		{ firstName: 'Nikolai', lastName: 'Davydenko', age: 12 },
+		{ firstName: 'Nils', lastName: 'Davydenko', age: 7 },
+		{ firstName: 'Mirza', lastName: 'Selimasic', age: 47 },
+		{ firstName: 'Nedisa', lastName: 'Selimasic', age: 13 },
+		{ firstName: 'Nedis', lastName: 'Selimasic', age: 11 },
+	];
+
+	$scope.employees = employees;
+}
+);
