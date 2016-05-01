6d6fb26db8a1d7d44c02f022a05bdbe72f08a0f0
MatsDrevstad <mats.drevstad@gmail.com>
2016-05-01 16:00:58 +0200

    ng-model

---
+++ AngularJS/scripts/Script05.js
@@ -0,0 +1,13 @@
+/// <reference path="angular.js" />
+
+//05.html
+//Create the module
+var myApp = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope) {
+        $scope.message = "I sommar och soliga dagar";
+
+        var employee = { firstName: 'Mats', lastName: 'Drevstad', age: 45 };
+        $scope.employee = employee;
+    }
+);
