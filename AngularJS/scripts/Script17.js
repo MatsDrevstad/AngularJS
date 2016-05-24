d871767bb39e74c8777fed5a44f7dea8f6f54ee1
matsd <matsd@DESKTOP-7J8IBRI>
2016-05-24 12:17:39 +0200

    17 Consuming Web Services

--- AngularJS/scripts/Script17.js
+++ AngularJS/scripts/Script17.js
@@ -5,16 +5,8 @@ var myApp = angular
     .module("myModule", [])
     .controller("myController", function ($scope, $http) {
 
-        $http.get("http://localhost:58564/WebService1.asmx/GetAllEmployees")
+        $http.get("WebService1.asmx/GetAllEmployees")
             .then(function (response) {
-    //            $scope.employees = [
-    //{ name: 'Mats Davidsson', dateOfBirth: new Date("January 6, 1971"), gender: 'Male', salary: '55555.65234' },
-    //{ name: 'Nikolai Dawsson', dateOfBirth: new Date("April 25, 2003"), gender: 'Male', salary: '44444' },
-    //{ name: 'Nils Dawsson', dateOfBirth: new Date("October 8, 2008"), gender: 'Male', salary: '33333.35' },
-    //{ name: 'Mirza Svensson', dateOfBirth: new Date("January 1, 1969"), gender: 'Female', salary: '55555.56' },
-    //{ name: 'Nedisa Svensson', dateOfBirth: new Date("December 20, 2002"), gender: 'Female', salary: '44444.64' },
-    //{ name: 'Nedis Svensson', dateOfBirth: new Date("December 22, 2005"), gender: 'Male', salary: '33333.00' },
-    //            ];
 
                 $scope.employees = response.data;
             });
